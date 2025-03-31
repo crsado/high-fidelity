fbb5601780e48e9ff2ca563fa70bb391377576ec
crsado <crs15@sfu.ca>
2025-03-30 21:14:57 -0700

    Added default last message

--- src/screens/Assistant/gpt.jsx
+++ src/screens/Assistant/gpt.jsx
@@ -26,7 +26,7 @@ const cache = await ai.caches.create({
 });
 console.log("Cache created:", cache);
 
-let lastMessage
+let lastMessage = "Hi, how can I help you today?"
 
 const ask = async function(input) {
     let parent = document.getElementById("message-box")
