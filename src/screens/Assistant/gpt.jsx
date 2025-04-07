7536236cb50581afaead3476fbfb0f1509989968
crsado <crs15@sfu.ca>
2025-04-06 20:41:43 -0700

    Ai doing weird output

--- src/screens/Assistant/gpt.jsx
+++ src/screens/Assistant/gpt.jsx
@@ -6,22 +6,23 @@ const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_KEY });
 
 // Change if hosted on web
 // Current website root url
-const url = window.location.origin + '/';
+const url = String(window.location.origin + '/');
 
 const modelName = "gemini-1.5-flash-002"
 
 const cache = await ai.caches.create({
     model: modelName,
     config: {
-        systemInstruction: "You are an interactive help system for the image manipulation software known as Pixlr." +
-            "This system is only a prototype as of now, so just give a plausible sounding explanation" +
-            "You must give a brief explanation of any features the user asks about." +
-            "You must attach a link to a relevant tutorial on the website following each response you give to a user's question." +
-            "If their question does not fit a specific category then provide a link to the tutorial home page." +
-            "Links you may attach are as follows:" +
-            "Tutorial home page: " + url + "tutorials" +
-            "Photo Editor Tutorial: " + url + "tutorials?page=editor" +
-            "AI Image Generator Tutorial: " + url + "tutorials?page=generator",
+        systemInstruction: "You are an interactive help system for the image manipulation software known as Pixlr. " +
+            "This system is only a prototype as of now, so just give a plausible sounding explanation. " +
+            "You must give a brief explanation of any features the user asks about. " +
+            "You must attach a link to a relevant tutorial on the website following each response you give to a user's question. " +
+            "If their question does not fit a specific category then provide a link to the tutorial home page. " +
+            "Add the links as strings, don't worry about making them functional. " +
+            "Links you may attach are as follows: " +
+            "Tutorial home page: " + url + "tutorials " +
+            "Photo Editor Tutorial: " + url + "tutorials?page=editor " +
+            "AI Image Generator Tutorial: " + url + "tutorials?page=generator ",
     },
 });
 console.log("Cache created:", cache);
